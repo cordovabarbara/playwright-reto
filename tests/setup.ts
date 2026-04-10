8e5227e5577621a069c59bf3ae78c06fed9de827
cordovabarbara <aliendocordova@gmail.com>
2026-04-10 08:56:12 -0500

    fix: generar cookies.json automaticamente si no existe

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,13 +1,18 @@
 import { chromium } from '@playwright/test';
+import fs from 'fs';
 
 async function globalSetup() {
+  // Si ya existe el archivo no lo regenera
+  if (fs.existsSync('cookies.json')) return;
+
   const browser = await chromium.launch();
   const page = await browser.newPage();
   
   await page.goto('https://www.bon-bonite.com/');
+  
+  // Esperar el popup y aceptar
   await page.getByRole('button', { name: 'Aceptar todo' }).click();
   
-  // Guarda el estado (cookies + localStorage)
   await page.context().storageState({ path: 'cookies.json' });
   await browser.close();
 }
